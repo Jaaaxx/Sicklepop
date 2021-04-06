673882d23c5b518324d461564cb43cc52617f697
Mr-Murkrow245 <loumansoccer@outlook.com>
2021-04-05 23:57:57 -0400

    added new news

--- data.js
+++ data.js
@@ -168,7 +168,8 @@ const newsData = [
     "Lick every last drop!",
     "Juicy!",
     "\"It's so big!\" (The Popsicle)",
-    "Suck out all the juices!"
+    "Suck out all the juices!",
+    "I can lick your popsicle all day!"
 ]
 
 const buildingsData = [
